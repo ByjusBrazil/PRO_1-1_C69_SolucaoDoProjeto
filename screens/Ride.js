40ac9bcf4c0140c8c7ba827bbb9cb5da48a9bcee
Thiago Saraiva <saraii_@live.com>
2022-02-09 14:42:42 -0300

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -18,9 +18,9 @@ export default class TransactionScreen extends Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
-        */
+      /*status === "granted" é verdadeiro se o usuário concedeu permissão
+        status === "granted" é falso se o usuário não concedeu permissão
+      */
       hasCameraPermissions: status === "granted",
       domState: domState,
       scanned: false
